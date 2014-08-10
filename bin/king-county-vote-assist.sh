5a76fec77765f4df2e4f36de46e773a909d062a8
Ian Leonard <antonlacon@gmail.com>
2014-08-09 23:47:35 -0700

    king-county-vote-assist: fix copyright header
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- bin/king-county-vote-assist.sh
+++ bin/king-county-vote-assist.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Merge King County ballot signature page with pages from original ballot to save on size and preserve clarity while emailing.
 #
-# Copyright 2012-2013 Ian Leonard <antonlacon@gmail.com>
+# Copyright 2012-2014 Ian Leonard <antonlacon@gmail.com>
 #
 # This file is king-county-vote-assist.sh.
 #
-# toggle-touchpad.sh is free software: you can redistribute it and/or modify
+# king-count-vote-assist.sh is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
 #
-# toggle-touchpad.sh is distributed in the hope that it will be useful,
+# king-count-vote-assist.sh is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with toggle-touchpad.sh. If not, see <http://www.gnu.org/licenses/>.
+# along with king-county-vote-assist.sh. If not, see <http://www.gnu.org/licenses/>.
 #
 # Order of pages for submission:
 # Cover Sheet
